206225be8bdf60d9ccacec7759e92e2cbdf659de
ManojKumarPradhan <mkpmca123@gmail.com>
2018-01-04 19:16:09 +0530

    Runnable

--- MultiThreading/src/main/java/com/bigob/MultiThreading/RunnableTest.java
+++ MultiThreading/src/main/java/com/bigob/MultiThreading/RunnableTest.java
@@ -2,4 +2,16 @@ package com.bigob.MultiThreading;
 
 public class RunnableTest {
 
+	public static void main(String[] args) {
+		Thread t1;
+		Runnable t2;
+		t1=new ThreadDemo("t1");
+		t1.start();
+		
+		t2=new ThreadDemo1();
+		//t2.run();
+		//we need to create a thread because we need to call start()
+		
+		new Thread(t2).start();
+	}
 }
---
+++ MultiThreading/src/main/java/com/bigob/MultiThreading/ThreadDemo1.java
@@ -0,0 +1,10 @@
+package com.bigob.MultiThreading;
+
+public class ThreadDemo1 implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("run method in Runnable implemented class");
+	}
+
+}

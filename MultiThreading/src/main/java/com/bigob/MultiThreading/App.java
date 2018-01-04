e9600ab99f9aa84621d5f11d1fab84dc6119d160
ManojKumarPradhan <mkpmca123@gmail.com>
2018-01-04 17:00:20 +0530

    MultiThreading Project Created

---
+++ MultiThreading/src/main/java/com/bigob/MultiThreading/App.java
@@ -0,0 +1,13 @@
+package com.bigob.MultiThreading;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

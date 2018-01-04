306f82d4227d6a6f6cae358699e9218cb2ec2acd
ManojKumarPradhan <mkpmca123@gmail.com>
2018-01-04 23:01:20 +0530

    Runnable

--- MultiThreading/src/main/java/com/bigob/MultiThreading/RunnableTest.java
+++ MultiThreading/src/main/java/com/bigob/MultiThreading/RunnableTest.java
@@ -5,13 +5,15 @@ public class RunnableTest {
 	public static void main(String[] args) {
 		Thread t1;
 		Runnable t2;
-		t1=new ThreadDemo("t1");
+		t1 = new ThreadDemo("t1");
 		t1.start();
-		
-		t2=new ThreadDemo1();
-		//t2.run();
-		//we need to create a thread because we need to call start()
-		
+
+		t2 = new ThreadDemo1();
+		/*
+		 * t2.run(); we need to create a thread because we need to call
+		 * start()	So Thread class have a parameter to pass the Runnable
+		 * interface object or it sub class object
+		 */
 		new Thread(t2).start();
 	}
 }

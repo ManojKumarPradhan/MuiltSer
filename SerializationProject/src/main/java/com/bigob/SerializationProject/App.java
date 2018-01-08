086e22b3315b60f2cd09e6f3472d14825b92c338
ManojKumarPradhan <mkpmca123@gmail.com>
2018-01-08 23:55:34 +0530

    Seri

--- SerializationProject/src/main/java/com/bigob/SerializationProject/App.java
+++ SerializationProject/src/main/java/com/bigob/SerializationProject/App.java
@@ -8,7 +8,13 @@ public class App implements Serializable{
 	 */
 	private static final long serialVersionUID = 9052185789211316239L;
 
+	public App() {
+		// TODO Auto-generated constructor stub
+		return;
+	}
+	
 	public static void main( String[] args ){
-    	
+    	System.out.println("Hi");
+    	return;
     }
 }
